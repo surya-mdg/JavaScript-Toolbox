c0d4ce189e6c56db7f97e4169b0e58d2d9479232
SMdG <72032559+Surya-mdg@users.noreply.github.com>
2023-06-16 20:02:04 +0530

    Add animation slider
    Adds a invisible mouse slider that animates the character.

--- Sliding-Window/slider.js
+++ Sliding-Window/slider.js
@@ -1,10 +1,43 @@
 const element = document.getElementById("window");
 const image = document.getElementsByTagName('img');
 
-const handleClick = (event) => {
-    console.log('Element clicked!');
-    image[0].setAttribute('src','assets/knight/run_2.png');
+const characters = [ "knight", "explorer"];
+const spriteRun1 = [ "run_0.png", "run_1.png", "run_2.png", "run_3.png", "run_4.png", "run_5.png", "run_6.png", "run_7.png", "run_8.png"];
+
+let mouseStart;
+let isClicked = false;
+let index = 0;
+let maxDistance = 270;
+let section = maxDistance/(spriteRun1.length);
+
+window.onmousedown = e => {
+    mouseStart = e.clientX - (section * index);
+    isClicked = true;
 }
 
-element.addEventListener('click',handleClick);
+window.onmouseup = e => {
+    isClicked = false;
+}
 
+window.onmousemove = e => {
+    if(!isClicked) return;
+
+    let distanceMoved = e.clientX - mouseStart;
+    if(distanceMoved >= maxDistance || distanceMoved <= -maxDistance)
+    {
+        mouseStart = e.clientX;
+        distanceMoved = e.clientX - mouseStart;
+    }
+
+    index = Math.floor(distanceMoved/section);
+    if(index >= 0)
+    {
+        const source = "assets/" + characters[0] + "/" + spriteRun1[index];
+        image[0].setAttribute('src',source);
+    }
+    else
+    {
+        const source = "assets/" + characters[0] + "/" + spriteRun1[spriteRun1.length + index];
+        image[0].setAttribute('src',source);
+    }
+}
