2b676982119e4936fae4bb3e69360509ea73073c
SMdG <72032559+Surya-mdg@users.noreply.github.com>
2023-07-05 23:59:50 +0530

    Add random card selection & choose mechanic

--- Card-Shuffle/shuffle.js
+++ Card-Shuffle/shuffle.js
@@ -1,13 +1,31 @@
 let container = document.getElementsByClassName("container");
 let cards = document.getElementsByClassName("table");
+let cardImages = document.getElementsByClassName("cards");
 
+let transSpeed = 0.25; //0.15
+let waitTime = 450; //300
+let shuffleRate = 100; //100
+
+let shuffle = true;
 let chosenCard;
+let cardSprites = {cover: "assets/card-cover.jpg", chosen: "assets/card-chosen.jpg", empty: "assets/card-empty.jpg"};
+chosenCard = Math.floor(Math.random() * 3);
+cardImages[chosenCard].id = "chosen";
+console.log('%c'+chosenCard, 'color: pink; background-color: black; width: 25px');
+cardImages[chosenCard].src = cardSprites.chosen;
 
 window.onclick = async () => {
-    chosenCard = Math.floor(Math.random() * 3);
-    console.log('%c'+chosenCard, 'color: pink; background-color: black; width: 25px');
-    cards = document.getElementsByClassName("table");
-    await Shuffle();
+    if(shuffle){
+        shuffle = false;
+        for(let i = 0; i < 3; i++)
+            cardImages[i].src = cardSprites.cover;
+        cards = document.getElementsByClassName("table");
+        await Shuffle();
+    }
+    else{
+        let chosenImage = document.getElementById("chosen");
+        chosenImage.src = cardSprites.chosen;
+    }
 }
 
 const Wait = async(delay) => {
@@ -42,12 +60,12 @@ const Shuffle = async () => {
         const pos1 = cards[card1].getBoundingClientRect().left;
         const movePos = pos1 - pos0;
 
-        cards[card0].style.transition = "transform 0.15s ease";
-        cards[card1].style.transition = "transform 0.15s ease";
+        cards[card0].style.transition = "transform " + transSpeed + "s ease";
+        cards[card1].style.transition = "transform " + transSpeed + "s ease";
         cards[card0].style.transform = 'translateX('+ movePos +'px)';
         cards[card1].style.transform = 'translateX('+ -movePos +'px)';
 
-        await Wait(300);
+        await Wait(waitTime);
 
         if(card0 === 0 && card1===2)
         {
@@ -65,6 +83,6 @@ const Shuffle = async () => {
         prev0 = card0;
         prev1 = card1;
 
-        await Wait(100);
+        await Wait(shuffleRate);
     }
 }
