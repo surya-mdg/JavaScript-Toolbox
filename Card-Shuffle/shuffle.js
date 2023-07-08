713bbbcb3ada5bf43711d9811890fab04ba20ea1
SMdG <72032559+Surya-mdg@users.noreply.github.com>
2023-07-08 19:40:17 +0530

    Add pixel art, comments & adjust values
    All the pixel art has been added.
    The values for shuffle have been adjusted to work properly.

--- Card-Shuffle/shuffle.js
+++ Card-Shuffle/shuffle.js
@@ -2,14 +2,15 @@ let container = document.getElementsByClassName("container");
 let cards = document.getElementsByClassName("table");
 let cardImages = document.getElementsByClassName("cards");
 
-let transSpeed = 0.25; //0.15
-let waitTime = 450; //300
-let shuffleRate = 100; //100
+let shuffleCount = 9; //Number of time cards should shuffle
+let transSpeed = 0.20; //Shuffle animation speed
+let waitTime = 400; //Time delay for shuffle animation to complete
+let shuffleRate = 80; //Time between each shuffle
 
 let shuffle = true;
 let chosenCard;
-let cardSprites = {cover: "assets/card-cover.jpg", chosen: "assets/card-chosen.jpg", empty: "assets/card-empty.jpg"};
-chosenCard = Math.floor(Math.random() * 3);
+let cardSprites = {cover: "assets/card-cover.png", chosen: "assets/card-chosen.png", empty: "assets/card-other.png"};
+chosenCard = Math.floor(Math.random() * 3); //Choose random card
 cardImages[chosenCard].id = "chosen";
 console.log('%c'+chosenCard, 'color: pink; background-color: black; width: 25px');
 cardImages[chosenCard].src = cardSprites.chosen;
@@ -17,7 +18,7 @@ cardImages[chosenCard].src = cardSprites.chosen;
 window.onclick = async () => {
     if(shuffle){
         shuffle = false;
-        for(let i = 0; i < 3; i++)
+        for(let i = 0; i < 3; i++) //Cover all cards and start shuffle
             cardImages[i].src = cardSprites.cover;
         cards = document.getElementsByClassName("table");
         await Shuffle();
@@ -28,7 +29,7 @@ window.onclick = async () => {
     }
 }
 
-const Wait = async(delay) => {
+const Wait = async(delay) => { //Delay
     await new Promise((resolve) =>{
         setTimeout(() => {
             resolve();
@@ -36,17 +37,17 @@ const Wait = async(delay) => {
     });
 }
 
-const Shuffle = async () => {
+const Shuffle = async () => { //Shuffle the cards
     let card0;
     let card1;
     let prev0;
     let prev1;
-    for(let i = 0; i < 10; i++)
+    for(let i = 0; i < shuffleCount; i++)
     {
         do{
             card0 = Math.floor(Math.random() * 3);
             card1 = Math.floor(Math.random() * 3);
-        }while(card0 === card1 || (card0 === prev0 && card1 === prev1));
+        }while(card0 === card1 || (card0 === prev0 && card1 === prev1)); //Generate two random indices until they are not same as prev iteration
 
         if(card1 < card0)
         {
@@ -58,7 +59,7 @@ const Shuffle = async () => {
         console.log({Card_0: card0, Card_1: card1});
         const pos0 = cards[card0].getBoundingClientRect().left;
         const pos1 = cards[card1].getBoundingClientRect().left;
-        const movePos = pos1 - pos0;
+        const movePos = pos1 - pos0; //Calculate the distance the cards should move
 
         cards[card0].style.transition = "transform " + transSpeed + "s ease";
         cards[card1].style.transition = "transform " + transSpeed + "s ease";
@@ -67,7 +68,7 @@ const Shuffle = async () => {
 
         await Wait(waitTime);
 
-        if(card0 === 0 && card1===2)
+        if(card0 === 0 && card1===2) //Change the order of divs in html according to shuffle
         {
             container[0].insertBefore(cards[2],cards[0]);
             container[0].insertBefore(cards[2],cards[1]);
@@ -77,8 +78,8 @@ const Shuffle = async () => {
 
         cards[card0].style.transition = "transform 0s ease";
         cards[card1].style.transition = "transform 0s ease";
-        cards[card0].style.transform = 'translateX('+ 0+'px)';
-        cards[card1].style.transform = 'translateX('+ 0+'px)';
+        cards[card0].style.transform = 'translateX('+ 0 +'px)';
+        cards[card1].style.transform = 'translateX('+ 0 +'px)';
 
         prev0 = card0;
         prev1 = card1;
